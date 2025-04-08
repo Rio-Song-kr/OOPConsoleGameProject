e1fa7bd80af0b98fb64fd36bfaea40ff65243eb0
riosong <rio.dw.song@gmail.com>
2025-04-08 12:38:26 +0900

    [Feat] 이전 Scene 저장 및 null 체크 추가 Scene 전환 시 어떤 씬에서 전환된 것인지에 따라 초기 설정을 달리 가져가기 위해 이전 씬 저장 기능 추가currentScene이 null이라면, Scene의 OnEnter() 메서드가 호출되지 않도록 추가

--- OOPConsoleGameProject/Manager/SceneManager.cs
+++ OOPConsoleGameProject/Manager/SceneManager.cs
@@ -5,17 +5,13 @@ public class SceneManager
     private static SceneManager _instance;
     private Dictionary<SceneName, Scene> _scenes;
 
+    private SceneName _previousScene;
+    public SceneName PreviousScene { get => _previousScene; }
     private Scene _currentScene;
 
-    public Scene CurrentScene
-    {
-        get { return _currentScene; }
-    }
+    public Scene CurrentScene { get => _currentScene; }
 
-    private SceneManager()
-    {
-        _scenes = new Dictionary<SceneName, Scene>();
-    }
+    private SceneManager() { _scenes = new Dictionary<SceneName, Scene>(); }
 
     public static SceneManager GetInstance()
     {
@@ -27,16 +23,21 @@ public class SceneManager
         return _instance;
     }
 
-    public void Add(SceneName sceneName, Scene scene)
-    {
-        _scenes.Add(sceneName, scene);
-    }
+    public void Add(SceneName sceneName, Scene scene) { _scenes.Add(sceneName, scene); }
 
     public void Move(SceneName sceneName)
     {
         if (_scenes.ContainsKey(sceneName))
         {
+            Console.Clear();
+            if (_currentScene != null)
+            {
+                _previousScene = PreviousScene;
+                _currentScene.OnExit();
+            }
+
             _currentScene = _scenes[sceneName];
+            _currentScene.OnEnter();
         }
     }
 }
