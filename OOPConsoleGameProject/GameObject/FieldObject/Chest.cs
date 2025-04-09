b525dc8b325480b2990d1f84b7443aeac7017a84
Rio_Song <rio.dw.song@gmail.com>
2025-04-10 03:18:57 +0900

    [Chore] Chest 기능은 마지막에 구현하기 위해 제외

--- OOPConsoleGameProject/GameObject/FieldObject/Chest.cs
+++ OOPConsoleGameProject/GameObject/FieldObject/Chest.cs
@@ -2,25 +2,24 @@
 
 public class Chest : FieldObject
 {
-    private static string _name { get; } = "Chest";
     private static int _chestIndex = 0;
 
-    public Chest(Vector2 position, ConsoleColor color = ConsoleColor.Blue) : base(color, '▣', position)
+    public Chest(string name, Vector2 position, ConsoleColor color = ConsoleColor.Blue) : base(color, '▣', position)
     {
-        //todo 상자는 문제를 출력(별도의 화면 필요)
-        //todo 문제를 맞추면 인벤토리에 있는 힌트 제거 및 키 추가
-        //todo 힌트 - 상자의 매칭 시스템이 필요함(ID 확인 등)
-        Name = _name;
+        Name = name;
         Index = _chestIndex++;
     }
 
     public override bool TryInteract(GameObject gameObject)
     {
-        //todo FieldObject는 Player 뿐만 아니라 Rock <-> Goal상호작용을 함
         if (gameObject is not Player player)
             return false;
 
+        //todo Vector2 값은 추후 InputManager의 입력과 연동이 되어야 함
+        //todo 문제를 맞추면 인벤토리에 있는 힌트 제거 및 키 추가
         //todo 돌과 플레이어가 부딪힐 경우 상호작용 추가
+        // GameManager.UI.PrintChest(new char[] { '1', '1', '1', '1' }, new Vector2(3, 0));
+
         return true;
     }
 }
